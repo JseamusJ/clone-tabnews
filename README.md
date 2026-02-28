b9f06c086fcb3843f5d8d747c633792347b724fd
James Johnson <jamespadilha@gmail.com>
2026-02-28 01:42:05 -0300

    Adicionando scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto do zero para treinar programação e implementar o https://www.tabnews.com.br no https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Paixão eu amo você😘 e as nossas filhotas🐶🐶</h1>
+  return <h1>Paixão eu amo você😘 e as nossas filhotas🐶🐶</h1>;
 }
 
 export default Home;
